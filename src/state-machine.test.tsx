73aca0e6031ba263fb072040bf399061c04958d8
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-06-05 11:43:30 -0700

    Add basic state machine tests

---
+++ src/state-machine.test.tsx
@@ -0,0 +1,24 @@
+import Machine from './state_machine';
+
+test('initial state', () => {
+  const machine = new Machine();
+
+  expect(machine.state).toEqual({
+    current: 'input1',
+    input1: null,
+    input2: null,
+    operand: null,
+    readout: null,
+  });
+});
+
+test('basic operations', () => {
+  const machine = new Machine();
+
+  machine.transition(2);
+  machine.transition('add');
+  machine.transition(2);
+  machine.transition('equals');
+
+  expect(machine.state.readout).toEqual(4);
+});
