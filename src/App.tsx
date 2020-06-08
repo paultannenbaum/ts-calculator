e35e82e8a4c6e59c06a14d39b25e6e930a355a8c
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-06-08 15:14:27 -0700

    Hook up handlers to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import StateMachine from './state-machine';
 
-const App = ({ readout = "0", current = "test" }) => {
+const calculator = new StateMachine()
+type operand = 'add' | 'subtract' | 'multiply' | 'divide' | 'equals';
+
+const App = () => {
+  const[state, setState] = useState(calculator.state)
+  const handleInput = (val: number | operand) => {
+    calculator.transition(val);
+    setState(calculator.state)
+  }
+
   return (
     <div className="container">
     <div className="calculator">
-      <input className="readout" disabled value={readout} />
+      <input className="readout" disabled value={state.readout} />
       <div className="keyboard">
         <div className="numbers">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className="zero">0</button>
+          <button onClick={() => handleInput(7)}>7</button>
+          <button onClick={() => handleInput(8)}>8</button>
+          <button onClick={() => handleInput(9)}>9</button>
+          <button onClick={() => handleInput(4)}>4</button>
+          <button onClick={() => handleInput(5)}>5</button>
+          <button onClick={() => handleInput(6)}>6</button>
+          <button onClick={() => handleInput(1)}>1</button>
+          <button onClick={() => handleInput(2)}>2</button>
+          <button onClick={() => handleInput(3)}>3</button>
+          <button className="zero" onClick={() => handleInput(0)}>0</button>
         </div>
         <div className="operands">
-          <button className="reset">C</button>
-          <button>+</button>
-          <button>-</button>
-          <button>×</button>
-          <button>÷</button>
-          <button className="equals">=</button>
+          <button className="reset" onClick={() => handleInput('equals')}>C</button>
+          <button onClick={() => handleInput('add')}>+</button>
+          <button onClick={() => handleInput('subtract')}>-</button>
+          <button onClick={() => handleInput('multiply')}>×</button>
+          <button onClick={() => handleInput('divide')}>÷</button>
+          <button className="equals" onClick={() => handleInput('equals')}>=</button>
         </div>
       </div>
     </div>
     <div className="state">
-      Current State: { current }
+      Current State: { state.current }
     </div>
   </div>
   );
