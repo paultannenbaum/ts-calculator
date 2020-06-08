093dbc817623ab9c82122c4f3d829a0bfb5b6f5e
Paul Tannenbaum <paultannenbaum@gmail.com>
2020-06-08 14:51:42 -0700

    Basic UI markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
-function App() {
+const App = ({ readout = "0", current = "test" }) => {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+    <div className="calculator">
+      <input className="readout" disabled value={readout} />
+      <div className="keyboard">
+        <div className="numbers">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button className="zero">0</button>
+        </div>
+        <div className="operands">
+          <button className="reset">C</button>
+          <button>+</button>
+          <button>-</button>
+          <button>×</button>
+          <button>÷</button>
+          <button className="equals">=</button>
+        </div>
+      </div>
+    </div>
+    <div className="state">
+      Current State: { current }
+    </div>
+  </div>
   );
 }
 
